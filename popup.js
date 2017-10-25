c70e53cf915a37899662afad95808f27a1fa73ef
Acreddy <sreedhar.a14@iiits.in>
2017-10-26 04:12:32 +0530

    added

---
+++ popup.js
@@ -0,0 +1,5 @@
+  var script = 'document.getElementsByName("fw_username")[0].value="sreedhar.a14";document.getElementsByName("fw_password")[0].value="fuck"';
+  chrome.tabs.executeScript({
+    code: script
+  });
+
